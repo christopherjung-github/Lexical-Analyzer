d05c3c3a6e72d79949e6baa6db9dd18a2a95ef61
Christopher Jung <cjung@csu.fullerton.edu>
2022-11-14 20:53:26 -0800

    Lexer function is completed.

---
+++ lexer.py
@@ -0,0 +1,66 @@
+import json
+import keyword
+
+op = "operator"
+key = "keyword"
+id = "identifier"
+real = "real"
+sep = "separator"
+
+operators = ['<', '>', '+', '-', '=']
+separator = ['(', ')', ';']
+keywords = ['while']
+
+def lexer(filename):
+
+    try:
+        with open(filename, 'r') as f:
+            display =   "token             lexeme   \n"
+            display +=  "----------------  ---------\n"
+            
+            new = ""
+            for line in f:
+                for character in line:
+                    if character != " ":
+                        #check if character before other characters is operator
+                        if character in operators and len(new) == 0:
+                            display +=  f"{op:18}{character:9}\n"
+                        #check if character before other characters is separator
+                        elif character in separator and len(new) == 0:
+                            display +=  f"{sep:18}{character:9}\n"
+                        #check if character after other characters is separator
+                        elif character in separator and len(new) != 0:
+                            if new.isdecimal() or new.isdigit():
+                                display +=  f"{real:18}{new:9}\n"
+                                new = ""
+                            elif new in key:
+                                display +=  f"{key:18}{new:9}\n"
+                                new = ""
+                            else:
+                                # display +=  f"{id:18}{new:9}\n"
+                                try:
+                                    float(new)
+                                    display +=  f"{real:18}{new:9}\n"
+                                    # new = ""
+                                except ValueError:
+                                    display +=  f"{id:18}{new:9}\n"
+                                    # new = ""
+                                new = ""
+                            display +=  f"{sep:18}{character:9}\n"
+                        else:
+                            new += character
+                    elif new != "" and new.isnumeric():
+                        display +=  f"{real:18}{new:9}\n"
+                        new = ""
+                    elif new != "" and keyword.iskeyword(new):
+                        display +=  f"{key:18}{new:9}\n"
+                        new = ""
+                    elif len(new) != 0:
+                        display +=  f"{id:18}{new:9}\n"
+                        new = ""      
+    except FileNotFoundError:
+        return "{filename} not found"
+    f.close()
+    return display
+
+print(lexer("input_scode.txt"))
